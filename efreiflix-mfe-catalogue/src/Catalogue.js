34bf39506f26dd95119ec27426fe92411db6f59f
Gaillard05 <maylisgaillard@hotmail.fr>
2025-02-14 11:10:45 +0100

    récupération des films via api themoviedb.org

--- efreiflix-mfe-catalogue/src/Catalogue.js
+++ efreiflix-mfe-catalogue/src/Catalogue.js
@@ -6,10 +6,26 @@ const Catalogue = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:2066/movies")
-      .then((res) => res.json())
-      .then((data) => setMovies(data))
-      .catch((err) => console.error("Erreur de chargement:", err));
+    const fetchCatalogue = async () => {
+      try {
+        const apiKey = '15d2ea6d0dc1d476efbca3eba2b9bbfb';
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=fr-FR`
+        );
+
+        const data = await response.json();
+
+        if (data.results) {
+          setMovies(data.results);
+        }
+      } catch (err) {
+        console.error("Erreur de chargement :", err);
+      }
+    };
+
+    fetchCatalogue();
+
+
   }, []);
 
   return (
@@ -25,17 +41,23 @@ const Catalogue = () => {
                 {/* Image du film */}
                 <img
                   className="object-cover cursor-pointer object-center h-full w-full rounded-xl transition-all duration-300"
-                  src={movie.posterUrl}
-                  alt={movie.name}
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt={movie.title}
                 />
 
                 {/* Description et bouton, masqués par défaut et affichés au survol */}
                 <div className="absolute inset-0 h-full w-full bg-black/80 text-white p-6 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                   <h2 className="text-2xl font-bold mb-2">{movie.title}</h2>
-                  <p className="text-sm text-gray-300 mb-2">{movie.year} | {movie.genres}</p>
-                  <p className="text-lg mb-4">{movie.description}</p>
-                  <p className="text-lg font-semibold mb-2 text-white">{movie.rating} / 10</p>
-                  <a href={movie.movieUrl} target="_blank" rel="noopener noreferrer">
+                  <p className="text-sm text-gray-300 mb-2">
+                    {movie.release_date ? movie.release_date.split("-")[0] : "Date inconnue"}
+                  </p>
+                  <p className="text-lg mb-4">{movie.overview.substring(0, 100)}...</p>
+                  <p className="text-lg font-semibold mb-2 text-white">{movie.vote_average} / 10</p>
+                  <a
+                    href={`https://www.themoviedb.org/movie/${movie.id}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <button className="my-2 bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full inline-flex items-center">
                       <span>Voir le film</span>
                     </button>
